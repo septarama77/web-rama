8d2d205e4c6909f39cf786498932531db085efe7
septarama77 <akunbuang707@gmail.com>
2026-01-07 12:55:36 +0700

    Add files via upload

---
+++ qgis2web_2025_12_19-12_23_16_036000/data/GerejaKat_4.js
@@ -0,0 +1,12 @@
+var json_GerejaKat_4 = {
+"type": "FeatureCollection",
+"name": "GerejaKat_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Gereja Kat": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.379121182520961, -6.984045338136595 ] } },
+{ "type": "Feature", "properties": { "id": null, "Gereja Kat": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.372934625441587, -6.995116491081196 ] } },
+{ "type": "Feature", "properties": { "id": null, "Gereja Kat": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.39521356745297, -6.994560547056882 ] } },
+{ "type": "Feature", "properties": { "id": null, "Gereja Kat": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.386901395344609, -6.964801712835034 ] } },
+{ "type": "Feature", "properties": { "id": null, "Gereja Kat": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.369656607464904, -6.961703358511493 ] } }
+]
+}
---
+++ qgis2web_2025_12_19-12_23_16_036000/data/Vihara_5.js
@@ -0,0 +1,11 @@
+var json_Vihara_5 = {
+"type": "FeatureCollection",
+"name": "Vihara_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Vihara": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.379470609467162, -6.987321215757231 ] } },
+{ "type": "Feature", "properties": { "id": null, "Vihara": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.399846434322242, -6.974237704390307 ] } },
+{ "type": "Feature", "properties": { "id": null, "Vihara": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.391157235127437, -6.979508877361576 ] } },
+{ "type": "Feature", "properties": { "id": null, "Vihara": null, "Jenis": null }, "geometry": { "type": "Point", "coordinates": [ 110.390570499148794, -6.962926393112892 ] } }
+]
+}
